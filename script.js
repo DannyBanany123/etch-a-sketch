452d098aed4d1fa365192b5c613130eba681bf5a
DannyBanany123 <ngodanny2@gmail.com>
2022-08-12 04:39:36 -0500

    Add eraser mode and color mode

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 const container = document.querySelector(".container");
 let toggle;
+let colorOn = true;
+let rainbowOn = false;
+
+const colorMode = document.querySelector(".color-mode");
+colorMode.setAttribute("id", "animate");
+const rainbowMode = document.querySelector(".rainbow-mode");
+const erase = document.querySelector(".eraser");
+
+colorMode.addEventListener("click", () => {
+    colorOn = true;
+    rainbowOn = false;
+    colorMode.setAttribute("id", "animate");
+    erase.removeAttribute("id");
+    rainbowMode.removeAttribute("id");
+});
+rainbowMode.addEventListener("click", () => {
+    colorOn = false;
+    rainbowOn = true;
+    rainbowMode.setAttribute("id", "animate");
+    colorMode.removeAttribute("id");
+    erase.removeAttribute("id");
+});
+erase.addEventListener("click", () => {
+    colorOn = false;
+    rainbowOn = false;
+    erase.setAttribute("id", "animate");
+    colorMode.removeAttribute("id");
+    rainbowMode.removeAttribute("id");
+    enable("white");
+});
+
+// Color input field
+const colorPick = document.querySelector(".rgb");
+const colorLabel = document.querySelector(".colorwrap");
+colorPick.addEventListener("change", () => {
+    colorLabel.style.backgroundColor = colorPick.value;
+    if (colorOn) {
+        enable(colorPick.value);
+    }        
+});
+
+// Create starting grid
+makeBoard(16);
+enable(colorPick.value);
+
+// Creates grid of given size
 function makeBoard(num) {
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
@@ -12,8 +58,6 @@ function makeBoard(num) {
     }
 }
 
-makeBoard(16);
-enable();
 // New board when user enters new size
 const inputSize = document.querySelector(".size");
 inputSize.addEventListener("change", () => {
@@ -22,38 +66,31 @@ inputSize.addEventListener("change", () => {
             container.firstElementChild.remove();
         }
         makeBoard(inputSize.value);
-        enable();
+        enable(colorPick.value);
     } else {
-        alert("Size must be between 8 and 100! Enter a new number.");
+        alert("Size must be between 8 and 100.\nEnter a new number.");
     }
 });
 
-// Color input field
-const colorPick = document.querySelector(".rgb");
-const colorLabel = document.querySelector(".colorwrap");
-colorPick.addEventListener("change", () => {
-    colorLabel.style.backgroundColor = colorPick.value;
+// Stop drawing when release mouse
+const body = document.querySelector("body");
+body.addEventListener("mouseup", () => {
+    toggle = false;
+});
+body.addEventListener("dragstart", () => {
+    toggle = false;
 });
 
 // Enable/Disable drawing on grid
-function enable() {
+function enable(color) {
     const buttons = document.querySelectorAll("#box");
     buttons.forEach((button) => {
         button.addEventListener("mousedown", () => {
             toggle = true;
-            button.style.backgroundColor = "black";
+            button.style.backgroundColor = color;
         });
         button.addEventListener("mouseover", () => {
-            if (toggle) button.style.backgroundColor = "black";
+            if (toggle) button.style.backgroundColor = color;
         });
-    });
-    
-    const body = document.querySelector("body");
-    body.addEventListener("mouseup", () => {
-        toggle = false;
-    });
-    
-    body.addEventListener("dragstart", () => {
-        toggle = false;
     });
 }
