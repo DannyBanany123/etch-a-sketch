6459dcfd2d99844fde195c4fb89d10bbea5b08d7
DannyBanany123 <ngodanny2@gmail.com>
2022-08-12 03:03:30 -0500

    Create button to resize grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const container = document.querySelector(".container");
+let toggle;
 function makeBoard(num) {
-    const container = document.querySelector(".container");
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     
@@ -12,6 +13,20 @@ function makeBoard(num) {
 }
 
 makeBoard(16);
+enable();
+// New board when user enters new size
+const inputSize = document.querySelector(".size");
+inputSize.addEventListener("change", () => {
+    if (inputSize.value <= 100 && inputSize.value >= 8) {
+        while (container.firstElementChild) {
+            container.firstElementChild.remove();
+        }
+        makeBoard(inputSize.value);
+        enable();
+    } else {
+        alert("Size must be between 8 and 100! Enter a new number.");
+    }
+});
 
 // Color input field
 const colorPick = document.querySelector(".rgb");
@@ -21,24 +36,24 @@ colorPick.addEventListener("change", () => {
 });
 
 // Enable/Disable drawing on grid
-let toggle;
-const buttons = document.querySelectorAll(".container > div");
-buttons.forEach((button) => {
-    button.addEventListener("mousedown", () => {
-        toggle = true;
-        button.style.backgroundColor = "black";
+function enable() {
+    const buttons = document.querySelectorAll("#box");
+    buttons.forEach((button) => {
+        button.addEventListener("mousedown", () => {
+            toggle = true;
+            button.style.backgroundColor = "black";
+        });
+        button.addEventListener("mouseover", () => {
+            if (toggle) button.style.backgroundColor = "black";
+        });
     });
-    button.addEventListener("mouseover", () => {
-        if (toggle) button.style.backgroundColor = "black";
+    
+    const body = document.querySelector("body");
+    body.addEventListener("mouseup", () => {
+        toggle = false;
     });
-});
-
-const body = document.querySelector("body");
-body.addEventListener("mouseup", () => {
-    toggle = false;
-})
-
-body.addEventListener("dragstart", () => {
-    toggle = false;
-})
-// end section
+    
+    body.addEventListener("dragstart", () => {
+        toggle = false;
+    });
+}
