bca48b3d115dadeb2055af6149d6ceabe87e3a1a
DannyBanany123 <ngodanny2@gmail.com>
2022-08-12 00:40:52 -0500

    Create grid, create function to draw on the grid

--- script.js
+++ script.js
@@ -6,8 +6,26 @@ function makeBoard(num) {
     for (let i = 0; i < (num * num); i++) {
         const div = document.createElement("div");
         div.setAttribute("id", "box");
+        div.style.userSelect = "none";
         container.appendChild(div);
     }
 }
 
 makeBoard(16);
+
+let toggle;
+const buttons = document.querySelectorAll(".container > div");
+buttons.forEach((button) => {
+    button.addEventListener("mousedown", () => {
+        toggle = true;
+        button.style.backgroundColor = "black";
+    });
+    button.addEventListener("mouseover", () => {
+        if (toggle) button.style.backgroundColor = "black";
+    });
+});
+
+const body = document.querySelector("body");
+body.addEventListener("mouseup", () => {
+    toggle = false;
+})
