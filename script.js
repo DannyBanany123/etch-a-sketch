4714e58829e35b557649fbb1c86b48132d41a2e0
DannyBanany123 <ngodanny2@gmail.com>
2022-08-11 23:07:15 -0500

    Create HTML shell, add primary CSS styling, create grid using JS

---
+++ script.js
@@ -0,0 +1,13 @@
+function makeBoard(num) {
+    const container = document.querySelector(".container");
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    
+    for (let i = 0; i < (num * num); i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "box");
+        container.appendChild(div);
+    }
+}
+
+makeBoard(16);
